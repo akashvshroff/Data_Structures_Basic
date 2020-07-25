3ee1ec4387c37f2b027cff8677ebb86596a2a7e8
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-07-25 11:38:31 +0530

    OOP implementation.

--- hash_table.py
+++ hash_table.py
@@ -1,10 +1,19 @@
-def get_hash(key):
-    """
-    Basic hash function that gets the mod(100) of the sum of ascii values of all
-    the characters of the key string. This allows a maximum size of the hash to
-    be 100 data objects.
-    """
-    s = 0  # sum of ascii
-    for char in key:
-        s += ord(char)
-    return s % 100
+class HashTable:
+    def __init__(self):
+        """
+        Constructor that defines the maximum array size and initialises the
+        array that is going to be used to store values for the hash table.
+        """
+        self.MAX = 100
+        self.hash_arr = [None for _ in range(self.MAX)]
+
+    def get_hash(self, key):
+        """
+        Basic hash function that gets the mod(100) of the sum of ascii values of all
+        the characters of the key string. This allows a maximum size of the hash to
+        be 100 data objects.
+        """
+        s = 0  # sum of ascii
+        for char in key:
+            s += ord(char)
+        return s % 100
