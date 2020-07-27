2109b9162617ec16c3f10403cc18a67a2bca9831
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-07-27 10:55:07 +0530

    Added random information for the tree.

--- trees.py
+++ trees.py
@@ -1,4 +1,4 @@
-class GeneralTree:
+class TreeNode:
     def __init__(self, data):
         """
         Initialises the attributes for each instance of the GeneralTree class.
@@ -13,3 +13,33 @@ class GeneralTree:
         """
         child.parent = self
         self.children.append(child)
+
+
+def main():
+    """
+    Tests the TreeNode class.
+    """
+    root = TreeNode('Books')
+
+    horror = TreeNode("Horror")
+    horror.add_child(TreeNode("The Shining"))
+    horror.add_child(TreeNode("1917"))
+    horror.add_child(TreeNode("American Gods"))
+
+    mystery = TreeNode("Mystery")
+    mystery.add_child(TreeNode("Poirot"))
+    mystery.add_child(TreeNode("Nancy Drew"))
+    mystery.add_child(TreeNode("Girl on the Train"))
+
+    emotive = TreeNode("Emotive")
+    emotive.add_child(TreeNode('Normal People'))
+    emotive.add_child(TreeNode('Abundance of Katherines'))
+    emotive.add_child(TreeNode('Conversations with Friends'))
+
+    root.add_child(mystery)
+    root.add_child(horror)
+    root.add_child(emotive)
+
+
+if __name__ == "__main__":
+    main()
