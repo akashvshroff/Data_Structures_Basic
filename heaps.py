1a37b4ebc77748548a97964304ea791e05a57d6d
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-07-28 13:59:31 +0530

    Implemented a min heap.

--- heaps.py
+++ heaps.py
@@ -27,43 +27,43 @@ class MinHeap:
         """
         If the current index has a left child.
         """
-        return get_left_child_index(index) < len(self.heap)
+        return self.get_left_child_index(index) < len(self.heap)
 
     def has_right_child(self, index):
         """
         If the current index has a right child.
         """
-        return get_right_child_index(index) < len(self.heap)
+        return self.get_right_child_index(index) < len(self.heap)
 
     def has_parent(self, index):
         """
         If the current index has a parent.
         """
-        return get_parent_index(index) < len(self.heap)
+        return self.get_parent_index(index) < len(self.heap)
 
     def get_left_child(self, index):
         """
         Gets the value of the left child.
         """
-        return self.heap[get_left_child_index(index)]
+        return self.heap[self.get_left_child_index(index)]
 
     def get_right_child(self, index):
         """
         Gets the value of the right child.
         """
-        return self.heap[get_right_child_index(index)]
+        return self.heap[self.get_right_child_index(index)]
 
     def get_parent(self, index):
         """
         Gets the value of the parent.
         """
-        return self.heap[get_parent_index(index))]
+        return self.heap[self.get_parent_index(index)]
 
     def swap_values(self, index1, index2):
         """
         Swaps the values at 2 given indices.
         """
-        self.heap[index1], self.heap[index2]=self.heap[index2], self.heap[index1]
+        self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
 
     def peek(self):
         """
@@ -77,8 +77,8 @@ class MinHeap:
         Removes the root i.e the min element from the array.
         """
         assert len(self.heap) > 0, "ERROR: Heap is empty."
-        item=self.heap[0]
-        self.heap[0]=self.heap.pop()
+        item = self.heap[0]
+        self.heap[0] = self.heap.pop()
         self.heapify_down()
         return item
 
@@ -94,23 +94,40 @@ class MinHeap:
         Start with the last element and walk up the heap and swap in case the
         elements are out of order.
         """
-        index=len(self.heap) - 1
+        index = len(self.heap) - 1
         while self.has_parent(index) and self.get_parent(index) > self.heap[index]:
             self.swap_values(self.get_parent_index(index), index)
-            index=self.get_parent_index(index)
+            index = self.get_parent_index(index)
 
     def heapify_down(self):
         """
         Start with the root element and keep walking down and fixing the heap
         while there are children.
         """
-        index=0
+        index = 0
         while self.has_left_child(index):
-            smaller_child_index=self.get_left_child_index(index)
+            smaller_child_index = self.get_left_child_index(index)
             if self.has_right_child(index) and self.get_right_child(index) < self.get_left_child(index):
-                smaller_child_index=self.get_right_child_index(index)
-            if items[index] < items[smaller_child_index]:
+                smaller_child_index = self.get_right_child_index(index)
+            if self.heap[index] < self.heap[smaller_child_index]:
                 break
             else:
                 self.swap_values(index, smaller_child_index)
-            index=smaller_child_index
+            index = smaller_child_index
+
+
+def main():
+    """
+    Tests the min heap class.
+    """
+    heap = MinHeap()
+    for i in range(10):
+        heap.add(i)
+    print(heap.peek())
+    for i in range(4):
+        heap.poll()
+    print(heap.peek())
+
+
+if __name__ == '__main__':
+    main()
